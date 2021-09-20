b69baf40ef52e40ac14aa7f00593d5f3fb09eafe
Leticia Fontoura <L.Wolff@ibm.com>
2021-09-20 19:18:28 -0300

    n

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var cookieParser = require('cookie-parser');
 
 var client_id = '0bb593d68f7c4e5fa695bed15a224234'; // Your client id
 var client_secret = '650a3db5c1474d88bcd7e283e3f80159'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = 'https://leticiasapp.herokuapp.com/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
